0a2e42d1784b15e98a11c20df27d24d5372ed7a4
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2023-09-12 17:36:13 +0200

    Progress toward receipt implementation

--- supermarket-receipt-java/src/main/java/Amount.java
+++ supermarket-receipt-java/src/main/java/Amount.java
@@ -1,4 +1,10 @@
 public record Amount(int cents) {
     public static final Amount ZERO = new Amount(0);
     public static final Amount ONE = new Amount(1);
+
+    @Override
+    public String toString() {
+        if (cents < 100) return "0." + cents;
+        return String.valueOf(cents);
+    }
 }
